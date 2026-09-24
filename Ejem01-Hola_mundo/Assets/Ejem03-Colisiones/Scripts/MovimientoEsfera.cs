7aff69f045ffa26361170ca8054e0f9393de1617
daniel <daniel.gonzalez.calvo@gregoriofer.com>
2026-09-24 12:11:00 +0200

    Subido proyecto con nuevos assets

---
+++ Ejem01-Hola_mundo/Assets/Ejem03-Colisiones/Scripts/MovimientoEsfera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MovimientoEsfera : MonoBehaviour
+{
+    [SerializeField]
+    float movH = Input.GetAxis("Horizontal");
+    float movV = Input.GetAxis("Vertical");
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
